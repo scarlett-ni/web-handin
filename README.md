901a3b73041fb33efa21a52c1d25b395b304f7e9
Scarlett Nihao <nihaoor@gmail.com>
2026-03-10 12:57:07 +0100

    created Routes

---
+++ README.md
@@ -0,0 +1,4 @@
+## Pages
+1. index.html: homepage and introduction.
+2. browse.html: where you can browse the existing appreciations.
+3. add.html: where you can add new appreciations on the wall.
---
+++ app.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import { logger } from './middlewares/logger.js'
+
+const app = express()
+const PORT = 3000
+
+app.use(logger)
+
+app.use('/assets', express.static('public'))
+app.use('/photos', express.static('public'))
+
+app.get('/', (request, response) => {
+    response.send('This is appreciation Wall @CODE!')
+})
+
+app.get('/posts', (request, response) =>{
+    response.send('Browse our appreciation wall')
+})
+
+app.get('/posts/:codeperson', (request, response) => {
+  const codeId = request.params.codeperson
+
+  response.send(`These are the posts for ${codeId}`)
+})
+
+app.get('/posts/:codeperson/:postId', (request, response) => {
+    const codeId = request.params.codeperson
+    const postId = request.params.postId
+
+  response.send(`This is an appreciation post for ${codeId} and the post ID is ${postId}.`)
+})
+app.get('/about', (request, response) =>{
+    response.send('Introducing Appreciation Wall @CODE')
+})
+
+app.get('/add', (request, response) =>{
+    const name = "CODEr"
+    const message = `Hello, ${name}!`
+
+    response.send(`${message} Write your appreciation for a CODE community member here!`)
+})
+
+app.post('/add', (request, response) =>{
+    response.send('Thank you for adding to the appreciation wall!')
+})
+
+app.listen(PORT, () => {
+  console.log(`👋 Started server on port ${PORT}`)
+})
