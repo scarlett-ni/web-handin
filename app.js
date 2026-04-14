e7c107d2d7c54a617e98af07df242064447bf7be
Scarlett Nihao <nihaoor@gmail.com>
2026-04-14 11:34:54 +0200

    added to index

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const Post = mongoose.model('Post', postSchema)
 
 
 const posts = [
-    {number: '1', slug: 'post-1'},
-    {number: '2', slug: 'post-2'}
+    {number: '1', slug: 'post-1', name: 'Tomas Pereda', message: 'Gracias! For everything, giving a smile every time I come to campus and laughing a little bit! and for your endless help around campus <3', from: 'Cristina'},
+    {number: '2', slug: 'post-2', name: 'Simon Gneuss, Lukas Kaiser, Laurin Notemann, Julian Gebhard', message: 'THANKS for the Insane Community work you put in', from:'Jesper'}
 ]
 
 posts.forEach(post => {
